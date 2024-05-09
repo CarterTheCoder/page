6f567f157d3e7d94b0dcfc804c5b9b1061636355
Carter <carter.bearboy@gmail.com>
2024-05-09 14:39:52 -0400

    Added joke when enabling light mode

--- darkMode.js
+++ darkMode.js
@@ -10,5 +10,11 @@ darkModeButton.addEventListener('click', function() {
 
 lightModeButton.addEventListener('click', function() {
     console.log('Light mode event triggered');
-    document.body.style.backgroundColor = 'white';
+    // Confirmation window for light mode (I am not responsible for eye damage)
+    confirmWindow = window.confirm('Are you sure you want to enable light mode? By continuing you agree that I cannot be held liable for spontaneous combustion of the eyes.')
+    if (confirmWindow) {
+        document.body.style.backgroundColor = 'white';
+    } else {
+        window.alert('Good choice.');
+    }
 });
