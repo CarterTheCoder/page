2eebf7d71096ac15e9217eeef2814dd9bc26a04a
Carter <carter.bearboy@gmail.com>
2024-05-10 12:38:24 -0400

    Improved light/dark mode button

--- darkMode.js
+++ darkMode.js
@@ -1,20 +1,13 @@
-// These are set to constants, because they should not change
-const darkModeButton = document.getElementById('darkModeButton');
-const lightModeButton = document.getElementById('lightModeButton');
 const body = document.body;
+const toggleButton = document.getElementById('darkModeButton');
 
-darkModeButton.addEventListener('click', function() {
-    console.log('Dark mode event triggered');
-    document.body.style.backgroundColor = 'black';
-});
-
-lightModeButton.addEventListener('click', function() {
-    console.log('Light mode event triggered');
-    // Confirmation window for light mode (I am not responsible for eye damage)
-    confirmWindow = window.confirm('Are you sure you want to enable light mode? By continuing you agree that I cannot be held liable for spontaneous combustion of the eyes.')
-    if (confirmWindow) {
-        document.body.style.backgroundColor = 'white';
+toggleButton.addEventListener('click', function() {
+    
+    if (body.style.backgroundColor === 'white') {
+        body.style.backgroundColor = 'black';
+        toggleButton.innerText = 'Switch to Light Mode'
     } else {
-        window.alert('Good choice.');
+        body.style.backgroundColor = 'white';
+        toggleButton.innerText = 'Switch to Dark Mode'
     }
-});
+  });
