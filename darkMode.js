c801396094d4efd09c6055e7de74e1dca8d60fe3
Carter <carter.bearboy@gmail.com>
2024-05-21 21:35:22 -0400

    Fix dark mode

--- darkMode.js
+++ darkMode.js
@@ -4,7 +4,7 @@ const toggleButton = document.getElementById('darkModeButton');
 toggleButton.addEventListener('click', function() {
     
     if (body.style.backgroundColor === 'white') {
-        body.style.backgroundColor = 'teal';
+        body.style.backgroundColor = 'black';
         toggleButton.innerText = 'Switch to Light Mode'
     } else {
         body.style.backgroundColor = 'white';
